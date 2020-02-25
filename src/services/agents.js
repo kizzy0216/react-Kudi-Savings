1ad814ca2c3c1f008d1e2abad32b3d85b3724902
Ph3bian <kudi@kudi-inc.local>
2020-02-25 18:48:24 +0100

    tests

--- src/services/agents.js
+++ src/services/agents.js
@@ -1,5 +1,5 @@
 import Axios, { MediaService } from 'utils/axios'
 
-export const createAgent = agent => Axios.post(`/agents/create`, agent)
+export const createAgent = agent => Axios.get(`/markets`, agent)
 
 export const uploadAvatar = agent => MediaService.post(`/images`, agent)
