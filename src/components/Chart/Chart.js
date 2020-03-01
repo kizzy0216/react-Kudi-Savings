505bcc213d92854d4b00deb32899c462b11fc7a1
Ph3bian <kudi@kudi-inc.local>
2020-03-01 20:17:24 +0100

    changed prettier tab width

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -7,40 +7,40 @@ import styles from './chart.module.scss'
 ReactFC.fcRoot(FusionCharts, Charts, FusionTheme)
 
 let dataSource = {
-    chart: {
-        showvalues: '0',
-        showlegend: '1',
-        showlabel: '1',
-        caption: '',
-        subcaption: '',
-        numberprefix: '',
-        numbersuffix: '',
-        plottooltext: '',
-        showhovereffect: '1',
-        yaxisname: '',
-        showsum: '0',
-        theme: 'fusion',
-        palettecolors: '29C3BE,00C588,18379A,8561C5,EE8728'
-    }
+  chart: {
+    showvalues: '0',
+    showlegend: '1',
+    showlabel: '1',
+    caption: '',
+    subcaption: '',
+    numberprefix: '',
+    numbersuffix: '',
+    plottooltext: '',
+    showhovereffect: '1',
+    yaxisname: '',
+    showsum: '0',
+    theme: 'fusion',
+    palettecolors: '29C3BE,00C588,18379A,8561C5,EE8728'
+  }
 }
 
 const Chart = props => {
-    const { chart, type, dataSource: data, height } = props
-    dataSource = { chart: { ...dataSource.chart, ...chart }, data }
-    const chartConfigs = {
-        type,
-        width: '100%',
-        height,
-        dataFormat: 'json',
-        dataLoadStartMessage: 'Please wait, chart is loading the data....',
-        dataSource
-    }
+  const { chart, type, dataSource: data, height } = props
+  dataSource = { chart: { ...dataSource.chart, ...chart }, data }
+  const chartConfigs = {
+    type,
+    width: '100%',
+    height,
+    dataFormat: 'json',
+    dataLoadStartMessage: 'Please wait, chart is loading the data....',
+    dataSource
+  }
 
-    return (
-        <section className={styles.Chart}>
-            <ReactFC {...chartConfigs} />
-        </section>
-    )
+  return (
+    <section className={styles.Chart}>
+      <ReactFC {...chartConfigs} />
+    </section>
+  )
 }
 
 export default Chart
