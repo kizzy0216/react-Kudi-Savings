b3e1b090cc64902c46c60db64ef96ee8c596026c
Ph3bian <kudi@kudi-inc.local>
2020-03-24 09:26:09 +0100

    format

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -38,31 +38,31 @@ const Layout = ({ children, auth }) => {
       title: 'Markets',
       link: '/markets',
       icon: <MarketsLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     },
     {
       title: 'Agents',
       link: '/agents',
       icon: <AgentsLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     },
     {
       title: 'Zonal Heads',
       link: '/zonal-heads',
       icon: <AgentsLink />,
-      userType: ['ADMIN',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'SUPER_ADMIN']
     },
     {
       title: 'Transactions',
       link: '/transactions',
       icon: <TransactionsLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     },
     {
       title: 'Customers',
       link: '/customers',
       icon: <CustomersLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     },
     // {
     //   title: 'Customer Insights',
@@ -74,19 +74,19 @@ const Layout = ({ children, auth }) => {
       title: 'Plans',
       link: '/plans',
       icon: <PlansLink />,
-      userType: ['ADMIN',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'SUPER_ADMIN']
     },
     {
       title: 'Cashout',
       link: '/cashout',
       icon: <CashoutLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     },
     {
       title: 'Settings',
       link: '/settings',
       icon: <SettingsLink />,
-      userType: ['ADMIN', 'ZONAL',"SUPER_ADMIN"]
+      userType: ['ADMIN', 'ZONAL', 'SUPER_ADMIN']
     }
   ]
   return (
