cc19b55debed034e3a6455a3a3da584b6bbf4de0
Dami Tesfaye <42331182+DamiTesfaye@users.noreply.github.com>
2020-10-02 19:19:50 +0100

    refactored the initial date and final date sent to filter agent wallet top up

--- src/pages/Agents/wallet-topUp.js
+++ src/pages/Agents/wallet-topUp.js
@@ -50,13 +50,19 @@ const WalletTopUp = props => {
       setStartDate(startDate)
       setFrom(
         moment(startDate)
-          .subtract(1, 'days')
+          .subtract(12, 'hours')
           .format('YYYY-MM-DD HH:mm:ss')
       )
     }
     if (endDate) {
       setEndDate(endDate)
-      setTo(moment(endDate).format('YYYY-MM-DD HH:mm:ss'))
+      setTo(
+        moment(endDate)
+          .add(11, 'hours')
+          .add(59, 'minutes')
+          .add(59, 'seconds')
+          .format('YYYY-MM-DD HH:mm:ss')
+      )
     }
     setShowReset(true)
   }
