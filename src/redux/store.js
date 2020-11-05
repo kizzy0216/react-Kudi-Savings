79bebf931858c730493a0c55a599add3d9aae4f3
Dami Tesfaye <damitesfaye@gmail.com>
2020-11-05 10:44:51 +0100

    redux-persist integrated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
 import RootReducer from './root-reducer'
+import storage from 'redux-persist/lib/storage'
 
-const Store = createStore(RootReducer)
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
+const persistedReducer = persistReducer(persistConfig, RootReducer)
+    
+const store = createStore(persistedReducer)
 
-export default Store
+export const persistor = persistStore(store)
+
+export default store
